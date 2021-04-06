0f93e4e73937d1e2cee068e8e15b246a5cded6bd
Philip Fowler <philipwfowler@gmail.com>
2021-04-06 13:13:48 +0100

    hmm

--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ from setuptools import setup
 from gumpy import __version__
 
 setup(
+    install_requires=[
+        "numpy >= 1.13",
+        "pytest >= 4.0.0",
+        "Biopython >= 1.70",
+        "tqdm >= 4.19.5",
+        "pysam >= 0.15.2"
+    ],
     name='gumpy',
     version=__version__,
     author='Philip W Fowler',
@@ -14,13 +21,6 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"],
     python_requires='>=3.5',
-    install_requires=[
-        "numpy >= 1.13",
-        "pytest >= 4.0.0",
-        "Biopython >= 1.70",
-        "tqdm >= 4.19.5",
-        "pysam >= 0.15.2"
-    ],
     license="University of Oxford, see LICENSE.md",
     scripts=['bin/gumpy-save-genome.py'],\
     zip_safe=False
