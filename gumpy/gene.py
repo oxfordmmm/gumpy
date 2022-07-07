c0d8e30efc315d8772d7f67eb70df5bea43720e3
JeremyWesthead <jeremywesthead3@gmail.com>
2022-07-07 16:57:44 +0100

    Added stop codon to valid amio acids fro valid variant

--- gumpy/gene.py
+++ gumpy/gene.py
@@ -350,9 +350,9 @@ class Gene(object):
         #Match amino acid SNP
         snp = re.compile(r"""
                     ([a-zA-Z_0-9]+@)? #Possibly leading gene name
-                    ([A-Z]) #Reference amino acid
+                    ([A-Z!]) #Reference amino acid
                     ([0-9]+) #Position
-                    ([A-Z]) #Alt amino acid
+                    ([A-Z!]) #Alt amino acid
                     """, re.VERBOSE)
         if self.codes_protein and snp.fullmatch(variant):
             #The variant is an amino acid SNP
