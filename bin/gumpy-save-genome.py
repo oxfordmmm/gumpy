abcd07bdf8e9b1b714a23b087934adc2091e2f9c
JeremyWesthead <jeremywesthead3@gmail.com>
2023-03-22 11:53:51 +0000

    Bump version + update gumpy-save-genome

--- bin/gumpy-save-genome.py
+++ bin/gumpy-save-genome.py
@@ -1,21 +1,31 @@
 #! /usr/bin/python3
-
+'''Load a gumpy Genome object, and use pickle to save to disk.
+Due to the security implications of the pickle module, USE AT YOUR OWN RISK. 
+It is recommended to avoid sending and recieving pickled objects.
+'''
 import argparse
+import pickle
+import os
 
 import gumpy
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--genbank_file",required=True,help="the path to a single VCF file")
-    parser.add_argument("--name",required=True,help="the path to a compressed gumpy Genome object")
+    parser.add_argument("--genbank_file",required=True,help="Path to a single genbank file")
+    parser.add_argument("--path",required=True,help="Path to the output file")
+    parser.add_argument("--overwrite", required=False, default=False, action='store_true', help="Overwrite a file at the given path (if exists)")
     options = parser.parse_args()
 
     # instantiate a gumpy Genome object using the provided Genbank file
     reference_genome=gumpy.Genome(options.genbank_file,show_progress_bar=True)
 
-    # save a compressed json to disc
-    # reference_genome.save_pickle(filename=options.name,compression=True,compresslevel=3)
-    reference_genome.save(options.name, compression_level=3)
+    if os.path.exists(options.path):
+        if options.overwrite:
+            print("File existed! Overwriting")
+            pickle.dump(reference_genome, open(options.path, "wb"))
+        else:
+            print("File already exists! Use --overwrite to replace it")
+    else:
+        pickle.dump(reference_genome, open(options.path, "wb"))
 
-    print(reference_genome)
